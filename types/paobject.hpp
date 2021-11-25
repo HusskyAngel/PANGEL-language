1352cd8a5df7534f783b823749d4f04a1a30372f
HusskyAngel <angel.juan@correounivalle.edu.co>
2021-11-25 16:36:01 -0500

    first day

--- types/paobject.hpp
+++ types/paobject.hpp
@@ -4,6 +4,15 @@
 #ifndef __PAOBJECT__HH__
 #define __PAOBJECT__HH__
 
+#define INT_TYPE 1
+#define DOUBLE_TYPE 2
+#define FLOAT_TYPE 3
+#define BOOL_TYPE 4
+#define STRING_TYPE 5
+#define DICTIONARY_TYPE 6
+#define ARRAY_TYPE 7
+#define LIST_TYPE 8
+
 class PAObject{
 	
 	public:
